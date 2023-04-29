583a33b018eeab30329663c67f79f07fbdb6c0a5
ShaiTea <109754435+ShaiTea@users.noreply.github.com>
2023-04-30 04:29:50 +0600

    Add files via upload

---
+++ src/main/java/com/backend/security/user/User.java
@@ -0,0 +1,69 @@
+package com.backend.security.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table
+
+public class User {
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String firstname;
+
+    private String lastname;
+
+    private String email;
+
+    private String password;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstname;
+    }
+
+    public void setFirstName(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastName() {
+        return lastname;
+    }
+
+    public void setLastName(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
